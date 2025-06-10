77c10cd647f4eb88cbf1500ec7eb7c56fe0536d3
Duza7090 <rafael.duza@gmail.com>
2025-06-10 12:42:33 -0300

    Adicionar arquivos de projeto.

---
+++ MinhaPrimeiraAPI/Data/PrimeiraAPIContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaPrimeiraAPI.Models;
+
+namespace MinhaPrimeiraAPI.Data
+{
+    public class PrimeiraAPIContext: DbContext
+    {
+        public PrimeiraAPIContext(DbContextOptions<PrimeiraAPIContext> options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Book>().HasData(
+            new Book
+            {
+                Id = 1,
+                Title = "The Great Gatsby",
+                Author = "F. Scott Fitzgerald",
+                YearPublished = 1925
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "The Great Gatsby",
+                Author = "F. Scott Fitzgerald",
+                YearPublished = 1925
+            }
+                );
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
